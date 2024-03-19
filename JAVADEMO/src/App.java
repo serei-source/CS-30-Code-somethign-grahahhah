52a71802d038308227a8dffcc5135c45ae6e270b
serei-source <sandyminds998@gmail.com>
2024-03-19 12:17:17 -0600

    fox3

---
+++ JAVADEMO/src/App.java
@@ -0,0 +1,13 @@
+// Assignment: Class Practice
+//Jian De Leon
+//March 19 2024
+//ur general introduction to things idk
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Computer Science 30");
+        System.out.println("My Name is Jian De Leon, and my favourite food is Sinigang.");
+        System.out.println("Java has all sorta of weird \"quirks\" which make displaying text a bit more challenging.\r\n" + //
+                        "Sometimes characters like parentheses (sometimes called brackets) don't work correctly.");
+    }
+}
---
+++ a1 character/sketch.js
@@ -0,0 +1,20 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  circle(width/2, height/2, 100,);
+  square(100,100,120);
+  line(160,220,160,400)
+  line()
+
+}
