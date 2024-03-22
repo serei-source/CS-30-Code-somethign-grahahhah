e6cb3227aba6472d64ca6cee49974766d833a3ad
serei-source <sandyminds998@gmail.com>
2024-03-22 11:55:47 -0600

    description introdution idk

---
+++ #5 Class Practice/#5 Class Practice/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Computer Science 30\r\n" + //
+                        "My Name is Jian De Leon and my favourite food is Sinigang.\r\n" + //
+                        "Java has all sorta of weird \"quirks\" which make displaying text a bit more challenging.\r\n" + //
+                        "Sometimes characters like parentheses (sometimes called brackets) don't work correctly.\r\n" + //
+                        "Escape sequences require us to use a backslash '\\' in order to output these special characters.");
+        System.out.println("");
+        System.out.println("Thank you for reading my description, and havea nice day!");
+    }
+}
