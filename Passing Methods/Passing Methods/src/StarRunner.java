d12554b2649abdeb52b3d63404863fbec0c403ae
serei-source <sandyminds998@gmail.com>
2024-04-16 11:59:54 -0600

    passing grahahahah

---
+++ Passing Methods/Passing Methods/src/StarRunner.java
@@ -0,0 +1,9 @@
+public class StarRunner {
+    public static void main(String[] args) throws Exception {
+        Starw quote = new Starw();
+
+        quote.Stelle();
+        quote.Blade();
+        quote.Jingyuan();
+    }
+}
---
+++ Passing Methods/Passing Methods/src/Starw.java
@@ -0,0 +1,15 @@
+public class Starw {
+
+    public void Stelle()
+    {
+        System.out.println("Reach the end of your story in your own way. \n When there is the chance to make a choice, make one that you know you won't regret.");
+    }
+    public void Jingyuan()
+    {
+        System.out.println("Life is like a finroll like two on a leaf, they're full of what they strive.");
+    }
+    public void Blade()
+    {
+        System.out.println("Out of 5 people, 3 must pay the price. You're not one of them; Jing yuan.");
+    }
+}
